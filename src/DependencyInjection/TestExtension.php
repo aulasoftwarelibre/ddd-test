a13362ba6c85361b5936825ed5add238db661c84
Sergio Gómez <sergio@uco.es>
2019-08-05 00:37:41 +0200

    Updated to Symfony Messenger 4.3

--- src/DependencyInjection/TestExtension.php
+++ src/DependencyInjection/TestExtension.php
@@ -15,15 +15,28 @@ namespace AulaSoftwareLibre\DDD\TestsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class TestExtension extends Extension
+final class TestExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
 
         $loader->load('services.xml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        if (!$container->hasExtension('framework')) {
+            throw new \LogicException('FrameworkBundle not found');
+        }
+
+        $config = [];
+        $config['messenger']['buses']['event.bus']['middleware'][] = 'aulasl.messenger_middleware.event_collector_plugin';
+
+        $container->prependExtensionConfig('framework', $config);
     }
 }
